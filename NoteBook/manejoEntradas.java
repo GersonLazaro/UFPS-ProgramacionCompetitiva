67dc88e01118fe883ee9801a6b6971b477049043
Gerson Lázaro <gersonlazaro@gersonlazaro.com>
2015-07-01 22:41:48 -0500

    Se realizan cambios sustanciales en Scanner sobreescrito. Tienen las pruebas basicas, falta verificar con pruebas exhaustivas

--- NoteBook/manejoEntradas.java
+++ NoteBook/manejoEntradas.java
@@ -1,28 +1,21 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
+public class Main {
 
-public class manejoEntradas {
-    
-    public static void main(String []args) throws IOException{
-        Scanner sc = new Scanner();
-        System.out.println(sc.nextBigInteger());
-        System.out.println(sc.nextBigDecimal());
-    }
-    
     static class Scanner{
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         StringTokenizer st = new StringTokenizer("");
-        
-        
+        int espacios = 0;
+
         public String nextLine() throws IOException{
-            if(st.hasMoreTokens()){
+        	if(espacios>0){
+            	espacios--;
+            	return "";
+            }else if(st.hasMoreTokens()){
                 StringBuilder salida = new StringBuilder();
                 while(st.hasMoreTokens()){
                     salida.append(st.nextToken());
@@ -34,60 +27,31 @@ public class manejoEntradas {
             }
             return br.readLine();
         }
-        
+
         public String next() throws IOException{
-            while ( !st.hasMoreTokens() ) {
+        	espacios=0;
+            while (!st.hasMoreTokens() ) {
                 st = new StringTokenizer(br.readLine() );
             }
             return st.nextToken();
         }
-        
+
         public boolean hasNext() throws IOException{
             while (!st.hasMoreTokens()) {
                 String linea = br.readLine();
                 if (linea == null) {
                     return false;
                 }
+                if(linea.equals("")){
+                	espacios++;
+                }
                 st = new StringTokenizer(linea);
             }
-            return st.hasMoreTokens();
+            return true;
         }
-        
-        public int nextInt() throws IOException {
-            return Integer.parseInt(next());
-        }
-        
-        public long nextLong() throws IOException {
-            return Long.parseLong(next());
-        }
-        
-        public double nextDouble() throws IOException {
-            return Double.parseDouble(next());
-        }
-        
-        public double nextFloat() throws IOException {
-            return Float.parseFloat(next());
-        }
-        
-        public double nextShort() throws IOException {
-            return Short.parseShort(next());
-        }
-        
-        public double nextByte() throws IOException {
-            return Byte.parseByte(next());
-        }
-        
-        public BigDecimal nextBigDecimal() throws IOException{
-            return new BigDecimal(next());
-        }
-        
-        public BigInteger nextBigInteger() throws IOException{
-            return new BigInteger(next());
-        }
-        
-        public StringBuilder nextStringBuilder() throws IOException{
-            return new StringBuilder(next());
-        }
-        
+
+         //public tipo nextTipoDato() throws IOException {
+         //	return Tipo.parseTipo(next());
+         //}
     }
 }
--- NoteBook/manejoEntradasComentado.java
+++ NoteBook/manejoEntradasComentado.java
@@ -1,35 +1,34 @@
-
-//Imports Obligatorios
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-//Imports opcionales: Solo se necesitan con los metodos nextBigDecimal y
-//nextBigInteger
-import java.math.BigDecimal;
-import java.math.BigInteger;
-
 
 /**
  * Sobreescritura de la libreria Scanner para mejorar la eficiencia
- * en eventos de programación competitiva. 
- * @version 3.0
+ * en eventos de programación competitiva.
+ * @version 4.0
+ * @author Bashkara's Formulae (team Candelaria):
+ * 		Gerson Lázaro - Melissa Delgado - Cristhian León
+ * 		UFPS
  */
-public class manejoEntradasComentado {
+public class Main {
 
-    
+
     static class Scanner{
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         StringTokenizer st = new StringTokenizer("");
-        
+        int espacios = 0;
         /**
          * Devuelve la siguiente linea entera en el stream.
          * @return String con la linea siguiente
-         * @throws IOException 
+         * @throws IOException
          */
         public String nextLine() throws IOException{
-            if(st.hasMoreTokens()){
+        	if(espacios>0){
+            	espacios--;
+            	return "";
+            }else if(st.hasMoreTokens()){
                 StringBuilder salida = new StringBuilder();
                 while(st.hasMoreTokens()){
                     salida.append(st.nextToken());
@@ -41,25 +40,26 @@ public class manejoEntradasComentado {
             }
             return br.readLine();
         }
-        
+
         /**
-         * Devuelve el siguiente token delimitado por espacios o saltos 
+         * Devuelve el siguiente token delimitado por espacios o saltos
          * de linea. Es decir, devuelve la siguiente palabra.
          * @return String con el siguiente token
-         * @throws IOException 
+         * @throws IOException
          */
-        public String next() throws IOException{            
-            while ( !st.hasMoreTokens() ) {
+        public String next() throws IOException{
+        	espacios=0;
+            while (!st.hasMoreTokens() ) {
                 st = new StringTokenizer(br.readLine() );
             }
             return st.nextToken();
         }
-        
+
         /**
          * Retorna true si hay mas tokens en el flujo de datos, o falso
          * en caso contrario.
          * @return true si y solo si hay mas tokens
-         * @throws IOException 
+         * @throws IOException
          */
         public boolean hasNext() throws IOException{
             while (!st.hasMoreTokens()) {
@@ -67,100 +67,25 @@ public class manejoEntradasComentado {
                 if (linea == null) {
                     return false;
                 }
+                if(linea.equals("")){
+                	espacios++;
+                }
                 st = new StringTokenizer(linea);
             }
-            return st.hasMoreTokens();
+            return true;
         }
-        
         /**
-         * Escanea el siguiente token del stream como un entero.
-         * REQUIERE LA ESCRITURA DEL MÉTODO NEXT
+         * Escanea el siguiente token del stream como el tipo de dato numerico
+         * requerido.
+         * Este tipo de dato puede ser int, long, short, byte, double o float
+         * IMPORTANTE: Este metodo es solo una plantilla, debe modificarse
+         * para ser utilizado.
+         * @require next()
          * @return int pasado por el input
-         * @throws IOException 
+         * @throws IOException
          */
-        public int nextInt() throws IOException {
-            return Integer.parseInt(next());
-        }
-        
-        /**
-         * Escanea el siguiente token del stream como un Long.
-         * REQUIERE LA ESCRITURA DEL MÉTODO NEXT
-         * @return long pasado por el input
-         * @throws IOException 
-         */
-        public long nextLong() throws IOException {
-            return Long.parseLong(next());
-        }
-        
-        /**
-         * Escanea el siguiente token del stream como un double.
-         * REQUIERE LA ESCRITURA DEL MÉTODO NEXT
-         * @return double pasado por el input
-         * @throws IOException 
-         */
-        public double nextDouble() throws IOException {
-            return Double.parseDouble(next());
-        }
-        
-        /**
-         * Escanea el siguiente token del stream como un float.
-         * REQUIERE LA ESCRITURA DEL MÉTODO NEXT
-         * @return float pasado por el input
-         * @throws IOException 
-         */
-        public float nextFloat() throws IOException {
-            return Float.parseFloat(next());
-        }
-        
-        /**
-         * Escanea el siguiente token del stream como un short.
-         * REQUIERE LA ESCRITURA DEL MÉTODO NEXT
-         * @return short pasado por el input
-         * @throws IOException 
-         */
-        public short nextShort() throws IOException {
-            return Short.parseShort(next());
-        }
-        
-        /**
-         * Escanea el siguiente token del stream como un byte.
-         * REQUIERE LA ESCRITURA DEL MÉTODO NEXT
-         * @return byte pasado por el input
-         * @throws IOException 
-         */
-        public byte nextByte() throws IOException {
-            return Byte.parseByte(next());
-        }
-        
-        /**
-         * Escanea el siguiente token del stream como un bigDecimal.
-         * REQUIERE LA ESCRITURA DEL MÉTODO NEXT
-         * @return bigDecimal pasado por el input
-         * @throws IOException 
-         */
-        public BigDecimal nextBigDecimal() throws IOException{
-            return new BigDecimal(next());
-        }
-        
-        /**
-         * Escanea el siguiente token del stream como un BigInteger.
-         * REQUIERE LA ESCRITURA DEL MÉTODO NEXT
-         * @return bigInteger pasado por el input
-         * @throws IOException 
-         */
-        public BigInteger nextBigInteger() throws IOException{
-            return new BigInteger(next());
-        }
-        
-        /**
-         * Escanea el siguiente token del stream como un StringBuilder.
-         * REQUIERE LA ESCRITURA DEL MÉTODO NEXT
-         * @return stringBuilder pasado por el input
-         * @throws IOException 
-         */
-        public StringBuilder nextStringBuilder() throws IOException{
-            return new StringBuilder(next());
-        }
-        
+         //public tipo nextTipoDato() throws IOException {
+         //	return Tipo.parseTipo(next());
+         //}
     }
 }
